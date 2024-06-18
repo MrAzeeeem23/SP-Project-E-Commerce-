e67b5423e118a584da703b0660ab33874131f8e1
AzeemKhan <azeemkh528@gmail.com>
2024-06-18 10:59:25 +0530

    frontend version 1

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createUser, loginUser, logoutCurrentUser, getAllUsers,  getCurrentUserProfile } from '../controllers/userController.js'
+import { createUser, loginUser, logoutCurrentUser, getAllUsers,  getCurrentUserProfile, updateCurrentUserProfile, deleteUserById, getUserById, updateUserById} from '../controllers/userController.js'
 import { authenticate, authorizeAdmin,} from '../middlewares/authMiddkewares.js'
 
 const router = express.Router()
@@ -8,7 +8,13 @@ router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers
 router.post('/auth', loginUser)
 router.post('/logout', logoutCurrentUser)
 
-router.route('/profile').get(authenticate, getCurrentUserProfile)
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile)
+
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navigation from './pages/Auth/Navigation.jsx'
+import {ToastContainer} from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
+
 
 function App() {
-
   return (
     <>
-    <h1>hello world this is azeem khan</h1>
+      <ToastContainer />
+      <Navigation />
+      <main className='py-3'>
+        <Outlet />
+      </main>
     </>
   )
 }
