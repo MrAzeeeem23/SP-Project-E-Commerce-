eb5490c2b3ed37fa6f2e96028eff42749f552b30
AzeemKhan <azeemkh528@gmail.com>
2024-07-04 12:49:13 +0530

    version 4.2.1

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 const connetDB = async () => {
     try {
-        // await mongoose.connect("mongodb+srv://azeemkh528:AzeemKH12@cluster0.lsj6hnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        await mongoose.connect("mongodb://127.0.0.1:27017/AzeemDB")
+        await mongoose.connect("mongodb+srv://azeemkh528:AzeemKH12@cluster0.lsj6hnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        // await mongoose.connect("mongodb://127.0.0.1:27017/AzeemDB")
         console.log("Successfully connected to the mogo")
     } catch (error) {
         console.log(error.message)
