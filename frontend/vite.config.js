8a39d890f80ca1bed2d9783d8cbe2bd7cbb0198d
AzeemKhan <azeemkh528@gmail.com>
2024-07-08 20:21:38 +0530

    host commit on version 5.1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   server: {
     //  host: true is used to run local host on your divice
-    // host: true,
+    host: true,
     proxy: {
       "/api/": "https://sp-project-e-commerce.onrender.com/",
       "/uploads/": "https://sp-project-e-commerce.onrender.com/",     
