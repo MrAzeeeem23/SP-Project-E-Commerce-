ebd83872ca001f3e90a65dda05bd790f766c8711
AzeemKhan <azeemkh528@gmail.com>
2024-07-09 12:27:54 +0530

    version 5.2.1

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -9,4 +9,4 @@ export const apiSlice = createApi({
   endpoints: () => ({}),
 });
 
-console.log(baseQuery)
+// console.log(baseQuery)
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  // base: '/',
   plugins: [react()],
   server: {
     //  host: true is used to run local host on your divice
