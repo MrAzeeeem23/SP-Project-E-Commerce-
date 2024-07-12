2b94691863c3a1c8b2f816c1a19dc5fa7eb1f7fa
Azeem Khan <97743706+MrAzeeeem23@users.noreply.github.com>
2024-07-12 23:48:51 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -8,8 +8,8 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
