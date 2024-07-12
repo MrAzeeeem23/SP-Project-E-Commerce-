c38c51192aa95577fcf0898177a8054cc57720de
Azeem Khan <97743706+MrAzeeeem23@users.noreply.github.com>
2024-07-12 22:54:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
