c349c676ff4ff1215898331b9fd5d41f636e14b9
Azeem Khan <97743706+MrAzeeeem23@users.noreply.github.com>
2024-07-12 14:55:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,16 +19,17 @@ connectDB();
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 // app.get('/api', (req, res) => {
 //     res.json({ message: 'This is CORS-enabled for all origins!' });
