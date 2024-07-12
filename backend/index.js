e43c736529acfc2df3938de9e67cfe5179698cfc
Azeem Khan <97743706+MrAzeeeem23@users.noreply.github.com>
2024-07-12 11:27:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'; // Import cors package
+// import cors from 'cors'; // Import cors package
 
 import connectDB from './config/db.js';
 import userRoute from './routes/userRoutes.js';
@@ -23,6 +23,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.get('/api/products/top', (req, res) => {
+    res.json({ message: 'This is CORS-enabled for all origins!' });
+});
+
+
 // Define routes
 app.use('/api/users', userRoute);
 app.use('/api/category', categoryRoutes);
@@ -33,7 +45,7 @@ app.use("/api/orders", orderRoutes);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
-app.use(cors());
+// app.use(cors());
 
 // app.get('/api/products/top', (req, res) => {
 //     res.setHeader('Access-Control-Allow-Origin', 'https://beatsstore.netlify.app');
