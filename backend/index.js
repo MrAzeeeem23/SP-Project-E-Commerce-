03b49ccaafb076dedf94e5b5a83bd7c5c37b2367
Azeem Khan <97743706+MrAzeeeem23@users.noreply.github.com>
2024-07-12 23:54:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 
 import connectDB from './config/db.js';
 import userRoute from './routes/userRoutes.js';
@@ -19,12 +19,17 @@ connectDB();
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://beatsstore.netlify.app'); // Allow all origins
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://beatsstore.netlify.app'); // Allow all origins
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
+
+app.use(cors({
+  origin: 'https://beatsstore.netlify.app', // replace with your frontend URL
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
