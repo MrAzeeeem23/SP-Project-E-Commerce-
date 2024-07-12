2959915be2c1902bfdfd2ccc7610107e136c4fbc
Azeem Khan <97743706+MrAzeeeem23@users.noreply.github.com>
2024-07-12 23:16:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,13 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
