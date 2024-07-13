853a8dadd109c936b27593023a3d5c03de41a06c
Azeem Khan <97743706+MrAzeeeem23@users.noreply.github.com>
2024-07-14 01:24:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors(req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://beatsstore.netlify.app'); // Allow all origins
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
