4c39feb7ae99815ba65f5fcf90b977b4e5aef5f3
Azeem Khan <97743706+MrAzeeeem23@users.noreply.github.com>
2024-07-14 01:05:51 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -1,17 +1,16 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
-  // Generate JWT token
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d", // Token expires in 30 days
+    expiresIn: "30d",
   });
 
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
-    httpOnly: false, // Prevents JavaScript access to the cookie
-    secure: process.env.NODE_ENV === "production", // Only use secure cookies in production
-    sameSite: "None", // Adjust based on your needs, 'None' allows cross-site cookies
-    maxAge: 30 * 24 * 60 * 60 * 1000, // Cookie expiration set to 30 days
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
   return token;
