d10ba4fd0888c2364034fd87fa0597e664a8d44d
Azeem Khan <azeemkh528@gmail.com>
2024-09-25 20:59:18 +0530

    updated git ignore file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose'
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const connetDB = async () => {
     try {
         // online mongoDB atles
 
-        await mongoose.connect("mongodb+srv://azeemkh528:AzeemKH12@cluster0.lsj6hnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-        // await mongoose.connect("mongodb+srv://azeemkhanbeing:AzeemKH12@cluster0.bye8fsl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.MONGO_URI)
 
         // offline mongoDB 
-        // await mongoose.connect("mongodb://127.0.0.1:27017/AzeemDB")
+        // await mongoose.connect(process.env.MONGO_URI_LOCAL)
+        
         console.log("Successfully connected to the mogo 🎉")
     } catch (error) {
         console.log(error.message)
